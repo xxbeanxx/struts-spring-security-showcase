efd1f5a53a3d8a06eacb5d289b01efde6de3c681
Greg Baker <greg.baker.bsc@gmail.com>
2015-11-08 15:24:18 -0330

    Updated pom.xml, added basic web.xml configuration, added index action.

---
+++ src/main/java/com/github/xxbeanxx/struts/actions/IndexAction.java
@@ -0,0 +1,11 @@
+package com.github.xxbeanxx.struts.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author Greg Baker
+ */
+@SuppressWarnings("serial")
+public class IndexAction extends ActionSupport {
+
+}
